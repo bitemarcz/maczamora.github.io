843dc8d7ef3320f54066c15c70765a20b6f5d645
Mario zamora <mario.zamora@keysight.com>
2021-08-09 19:50:22 -0600

    Added in blog post stuff

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -20,50 +20,43 @@
         <!-- <h4 class="career">Career Background</h4> -->
         <b-col></b-col>
         <b-col cols="10">
-          <h1 class="heading">What is Counter Comms?</h1>
+          <h1 class="heading">Posts</h1>
+        </b-col>
+        <b-col><BlogPostList :posts="$page.posts.edges"/></b-col>
+      </b-row>
+      <b-row class="pb-5 pt-5">
+        <b-col></b-col>
+        <b-col cols="10">
         </b-col>
         <b-col></b-col>
       </b-row>
       <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-          Counter-Comms is a Cybersecurity consulting firm built to deliver results, create value for your business, protect and defend you from digital cyber threats, and educate our partners, clients, and you about Cybersecurity. Our motto here at Counter-Comms, "No business left behind."
-        </b-col>
-        <b-col></b-col>
-      </b-row>
-      <b-row class="pb-5 pt-5">
-        <b-col></b-col>
-        <b-col cols="10">
-            With the most recent ransomware events, black hat hackers are gaining speed and traction in supply chain attacks. They won't stop at anything to get what they want. From nation-states to script kiddies and scammers, there is no slowing down for the enemy. Even some of the most robust enterprises can't keep up with company-wide demand and protection, so they need to distribute protective mechanisms and education for their employees to protect themselves. You may ask yourself, when will it end? What more can I do? How do I protect my assets? That's where Counter-Comms comes in.
         </b-col>
         <b-col></b-col>
       </b-row>
        <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-            We are building a world-class team of consultants, both new and experienced, who have a passion and education for Cybersecurity, Privacy, and Threat Intelligence. Our team consists of a single full-time Software Engineer working on Smarter-IT initiatives, automation, and building scalable, reliable, bug-free Software with a background in Cybersecurity Defense designated by the NSA and The Department of Homeland Security. Counter-Comms hopes to expand the team as our growth continues both with clients and partners. Besides the currently listed service offerings, we also can offer services in the following areas: Networking, Automation, Docker and Kubernetes, Cloud Technologies - AWS or Azure. These will be evaulated on a case-by-case basis. Contact us for more information.
-          <form action="/contact"><input type="submit" value="Get in Contact" /></form>
         </b-col>
         <b-col></b-col>
       </b-row>
        <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-          <h1 class="heading">The Brand</h1>
         </b-col>
         <b-col></b-col> 
       </b-row>
       <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-          <g-image class="brand" src="~/Capshure-Sec-v1.6.png" alt="Responsive Logo"/>
         </b-col>
         <b-col></b-col> 
       </b-row>
        <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-          The Counter-Comms logo represents the entire lifecycle of Cybersecurity. The red color represents offense, and the blue color represents defense. The white squares represent a keyboard and ethics that an ethical hacker must uphold. We like to call these sophisticated minds white-hat hackers. The gold represents the amount of money that is involved with this industry. Counter-Comms was built with the idea to have a recognized identity within the community, our clients, and our partners. We hope you enjoy learning about our brand and identity as we continue our work with you. 
         </b-col>
         <b-col></b-col>
       </b-row>
@@ -75,27 +68,12 @@
        <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col cols="10">
-          <h1 class="heading">Our Team</h1>
         </b-col>
         <b-col></b-col>
       </b-row>
        <b-row class="pb-5 pt-5">
         <b-col></b-col>
         <b-col >
-          <div class="info-person-1">
-            <b-card
-              title="Lead Cybersecurity Consultant"
-              sub-title="Mario Zamora"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 30rem;"
-              class="mb-2"
-            >
-              <b-button href="/services" variant="primary">Services</b-button>
-            </b-card>
-          </div>
         </b-col>
         <b-col></b-col>
       </b-row>
@@ -111,9 +89,16 @@
 </template>
 
 <script>
+
+import BlogPostList from '~/components/BlogPostList.vue';
+
 export default {
   metaInfo: {
-    title: "About",
+    title: "Blog",
+  },
+
+  components: {
+    BlogPostList
   },
     data() {
       return {
@@ -122,6 +107,20 @@ export default {
     }
   };
 </script>
+
+<page-query>
+query {
+  posts: allPost {
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</page-query>
 
 <style>
 
